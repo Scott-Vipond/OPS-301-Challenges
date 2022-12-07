ce6087ef535f530596abbfa0eca2b65e8c7057b6
LabServer <scott.donna.vipond@outlookcom>
2022-12-07 16:21:33 +0000

    Created Readme and Hello script

---
+++ hello/hello.py
@@ -0,0 +1,12 @@
+# Not sure what is used here for Python scripts!
+
+# Script: Python setup
+# Author: Scott Vipond                      
+# Date of latest revision: 7 Dec 22
+# Purpose: OPS-301 Python setup Ubuntu VM
+
+# Basic function
+msg= "Hello World"
+print(msg)
+
+# End
