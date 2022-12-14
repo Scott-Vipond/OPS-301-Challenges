77aab206772d64c04d5da7a6c8c3a23483d41966
LabServer <scott.donna.vipond@outlookcom>
2022-12-14 02:35:00 +0000

    Demo saved

--- OPS-301-Challenge02.sh
+++ OPS-301-Challenge02.sh
@@ -13,21 +13,22 @@
 
 # Reference sites below:
 # https://www.cyberciti.biz/faq/unix-linux-getting-current-date-in-bash-ksh-shell-script/
+# https://replit.com/@HexxKing1/Ops-301d7-Challenge-02#main.sh
 
-# Variables for the different portions of the date and time needed for script
+# Variables for the different portions of the date and time needed for script, none were used in script
 year=`date +%Y`
 month=`date +%m`
 day=`date +%d`
 hour=`date +%H`
 minute=`date +%M`
 second=`date +%S`
+
 # Test echo commands to ensure above variables function as expected
 # echo `date`
 # echo "Current Date: $day-$month-$year"
 # echo "Current Time: $hour:$minute:$second"
 
-# Main code goes here
-# copies the syslog to the current directory
+# Copies the syslog to the current directory
 cp /var/log/syslog .
 
 # Appends the above syslog in current directory with the current date and time
